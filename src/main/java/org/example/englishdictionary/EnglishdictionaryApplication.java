a13123c769ac70c8a9176d1b581f45171938c102
dson <ndson03@gmail.com>
2024-11-10 23:13:46 +0700

    create project

---
+++ src/main/java/org/example/englishdictionary/EnglishdictionaryApplication.java
@@ -0,0 +1,13 @@
+package org.example.englishdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishdictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishdictionaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/englishdictionary/EnglishdictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.englishdictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishdictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
